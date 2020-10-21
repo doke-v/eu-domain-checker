da374feeb4fc7ae45574be547686ae7d27e738b5
tuhnik <huntlarsen@gmail.com>
2020-10-22 00:04:37 +0300

    basic formatting

--- index.js
+++ index.js
@@ -6,45 +6,44 @@ const DEFAULT_CRON_EXPRESSION = '0 0 * * *';
 const DEFAULT_STATUS = 'AVAILABLE';
 
 class EuDomainChecker {
-    constructor(domain) {
-        if (!domain) {
-            throw new Error('Missing domain name.');
-        }
-        this.domain = encodeURIComponent(domain);
+  constructor(domain) {
+    if (!domain) {
+      throw new Error('Missing domain name.');
     }
+    this.domain = encodeURIComponent(domain);
+  }
 
-    fetchDomainData() {
-        return axios.get(URL + this.domain).then(({ data }) => data);
-    }
+  fetchDomainData() {
+    return axios.get(URL + this.domain).then(({ data }) => data);
+  }
 
-    check({
-        status = DEFAULT_STATUS,
-        cronExpression = DEFAULT_CRON_EXPRESSION,
-    }) {
-        return new Promise((resolve) => {
-            const job = new CronJob(cronExpression, () => {
-                this.fetchDomainData()
-                    .then(data => {
-                        if (data.status === status) {
-                            job.stop();
-                            resolve(data);
-                        }
-                    })
-                    .catch(e => {
-                        console.error(e.message);
-                    });
-            });
-            job.start();
-        });
-    }
+  check({ status = DEFAULT_STATUS, cronExpression = DEFAULT_CRON_EXPRESSION }) {
+    return new Promise(resolve => {
+      const job = new CronJob(cronExpression, () => {
+        this.fetchDomainData()
+          .then(data => {
+            if (data.status === status) {
+              job.stop();
+              resolve(data);
+            }
+          })
+          .catch(e => {
+            console.error(e.message);
+          });
+      });
+      job.start();
+    });
+  }
 
-    checkOnce() {
-        return this.fetchDomainData();
-    }
+  checkOnce() {
+    return this.fetchDomainData();
+  }
 
-    isAvailable() {
-        return this.fetchDomainData().then(({ status }) => status === DEFAULT_STATUS);
-    }
+  isAvailable() {
+    return this.fetchDomainData().then(
+      ({ status }) => status === DEFAULT_STATUS
+    );
+  }
 }
 
 module.exports = EuDomainChecker;
